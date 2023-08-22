2e25a66985efe2cc595d21baa883989176e92500
alessandro <alessandrodamata6@gmail.com>
2023-08-22 16:11:02 -0300

    useRef e DOM

--- src/components/HookUseState/HookUseRef.jsx
+++ src/components/HookUseState/HookUseRef.jsx
@@ -8,6 +8,18 @@ const HookUseRef = () => {
 
     useEffect(() => {
         numberRef.current = numberRef.current + 1
+    })
+
+    // 2 - USEREF E DOM
+    const inputRef = useRef()
+    const [text, setText] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setText("")
+
+        inputRef.current.focus()
+    }
     return(
         <div>
             <h2>useRef</h2>
@@ -15,7 +27,15 @@ const HookUseRef = () => {
                 <p>Couter 1: {counter}</p>
                 <button onClick={() => setCounter(anterior => anterior + 1)}>Conatdor A</button>
                 <p>Couter 2: {counterB}</p>
-                <button onClick={() => setCounterB(anterior => anterior + 1)}>Conatdor B</button>
+                <button style={{marginBottom: 15}} onClick={() => setCounterB(anterior => anterior + 1)}>Conatdor B</button>
+
+                {/* 2 - USEREF E DOM */}
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        <input type="text" ref={inputRef} value={text} onChange={e => setText(e.target.value)} />
+                    </label>
+                    <input type="submit" value={"Enviar"} />
+                </form>
             <hr />
         </div>
     )
