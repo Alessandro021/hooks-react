3546c0c3295c2261390cb6530383858ff31995a5
alessandro <alessandrodamata6@gmail.com>
2023-08-22 10:24:46 -0300

    useEffect utilizando o array de dependencias

--- src/components/HookUseState/HookUseEffect.jsx
+++ src/components/HookUseState/HookUseEffect.jsx
@@ -18,11 +18,24 @@ const HookUseEffect = () => {
         //EXECULTA SOMENTE UMA VEZ
         console.log("useEffect: SEREI EXECULTADO SOMENTE UMA VEZ")
     },[])
+
+
+    // 3 - ITEM NO ARRAY DE DEPENDENCIAS DO USEEFFECT
+    const [anotherNumber, setAnotherNumber] = useState(0)
+
+    useEffect(() => {
+        if(anotherNumber !== 0){
+            console.log("useEffect: SO SEREI EXECULTADO QUANDO A STATE 'anotherNumber' FOR ALTERARDA")
+        }
+    },[anotherNumber])
     return(
         <div>
             <h2>useEffect</h2>
             <p>Number: {number}</p>
             <button onClick={changeSomething}>clicar</button>
+
+            <p>AnotherNumber: {anotherNumber}</p>
+            <button onClick={() => setAnotherNumber(anterior => anterior + 1)}>alterar anotherNumber</button>
             <hr />
         </div>
     )
